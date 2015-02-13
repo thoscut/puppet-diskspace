cc7c167ba96b51290ea01a6b40cf228b59483e55
WhatsARanjit <whatsaranjit@gmail.com>
2015-02-13 16:22:51 +0000

    Can't use match method on windows

--- lib/facter/diskspace.rb
+++ lib/facter/diskspace.rb
@@ -1,5 +1,7 @@
 require 'facter'
 
+$supported_os = [ 'Linux', 'AIX', 'Darwin' ]
+
 case Facter.value(:kernel)
 when 'Linux','AIX'
   df      = '/bin/df -P'
@@ -13,22 +15,24 @@ when 'Darwin'
   umatch  = 5
 end
 
-mounts = Facter::Util::Resolution.exec(df)
-mounts_array = mounts.split("\n")
-mounts_array.each do |line|
-  m = /#{pattern}/.match(line)
-  if m
-    fs = m[dmatch].gsub(/^\/$/, 'root')
-    fs = fs.gsub(/[\/\.:\-]/, '')
-    Facter.add("diskspace_#{fs}") do
-      confine :kernel => [ 'Linux', 'AIX', 'Darwin' ]
-      setcode do
-        m[umatch].to_i
+if $supported_os.include? Facter.value(:kernel)
+  mounts = Facter::Util::Resolution.exec(df)
+  mounts_array = mounts.split("\n")
+  mounts_array.each do |line|
+    m = /#{pattern}/.match(line)
+    if m
+      fs = m[dmatch].gsub(/^\/$/, 'root')
+      fs = fs.gsub(/[\/\.:\-]/, '')
+      Facter.add("diskspace_#{fs}") do
+        confine :kernel => $supported_os
+        setcode do
+          m[umatch].to_i
+        end
       end
-    end
-    Facter.add("diskspacefree_#{fs}") do
-      setcode do
-        100 - m[umatch].to_i
+      Facter.add("diskspacefree_#{fs}") do
+        setcode do
+          100 - m[umatch].to_i
+        end
       end
     end
   end
