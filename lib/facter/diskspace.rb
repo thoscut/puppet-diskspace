de6a670cf3a3d6e135ba603c08e8855f8ba7405a
WhatsARanjit <whatsaranjit@gmail.com>
2015-02-14 15:34:31 +0000

    Added support for Windows using wmic.exe

--- lib/facter/diskspace.rb
+++ lib/facter/diskspace.rb
@@ -1,8 +1,9 @@
 require 'facter'
 
-$supported_os = [ 'Linux', 'AIX', 'Darwin' ]
+$supported_os = [ 'Linux', 'AIX', 'Darwin',  'windows' ]
+kernel = Facter.value(:kernel)
 
-case Facter.value(:kernel)
+case kernel
 when 'Linux','AIX'
   df      = '/bin/df -P'
   pattern = '^([/\w\-\.:]+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)%\s+([/\w\-\.:]+)'
@@ -13,28 +14,39 @@ when 'Darwin'
   pattern = '^(?:map )?([/\w\-\.:\-]+)\s+(\d+)\s+(\d+)\s+(\d+)\s+(\d+)%\s+([/\w\-\.:]+)'
   dmatch  = 9
   umatch  = 5
+when 'windows'
+  df      = 'C:\Windows\System32\wbem\WMIC.exe logicaldisk get deviceid,freespace,size'
+  pattern = '^([A-Z]:)\s+(\d+)\s+(\d+)'
+  dmatch  = 1
 end
 
-if $supported_os.include? Facter.value(:kernel)
+if $supported_os.include? kernel
   mounts = Facter::Util::Resolution.exec(df)
-  mounts_array = mounts.split("\n")
+  mounts_array = mounts.split(/[\r\n]+/)
   mounts_array.each do |line|
     m = /#{pattern}/.match(line)
     if m
       fs = m[dmatch].gsub(/^\/$/, 'root')
       fs = fs.gsub(/[\/\.:\-]/, '')
+      if kernel == 'windows' 
+        # Windows doesn't report percentages but bytes
+        # Choosing to do the math and round down
+        used = ((1 - (m[2].to_f/m[3].to_f))*100).floor
+      else
+        used = m[umatch].to_i
+      end
       Facter.add("diskspace_#{fs}") do
         confine :kernel => $supported_os
         setcode do
-          m[umatch].to_i
+          used
         end
       end
       Facter.add("diskspacefree_#{fs}") do
+        confine :kernel => $supported_os
         setcode do
-          100 - m[umatch].to_i
+          100 - used
         end
       end
     end
   end
 end
-
